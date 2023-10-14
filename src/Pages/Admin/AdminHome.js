efc9142a5ea8b0b690944994648e6705e8b55c6e
Tribhuwan <tribhuwanja@gmail.com>
2023-10-14 14:37:57 +0530

    AdminHome Something unuseful content  deleted

--- src/Pages/Admin/AdminHome.js
+++ src/Pages/Admin/AdminHome.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
-import NewAdminData from "./NewAdminData";
 import Appp from "../../adminComponents/Appp";
 // import { FaAlignCenter } from "react-icons/fa";
 const AdminHome = () => {
